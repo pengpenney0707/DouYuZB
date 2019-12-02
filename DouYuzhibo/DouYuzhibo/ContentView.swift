c23800c4122a36f3217441e48631606c03a0a97e
pengpenney0707 <pengpenney0707@gmail.com>
2019-12-02 11:03:15 +0800

    初始化项目

---
+++ DouYuzhibo/DouYuzhibo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DouYuzhibo
+//
+//  Created by pengxy on 2019/12/2.
+//  Copyright © 2019 penneypeng.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
